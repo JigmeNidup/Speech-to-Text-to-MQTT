2b161f97a02929cdc923c2ef67cc3676dd563269
Jigme Nidup <jigmenidup8102003@gmail.com>
2024-04-09 01:37:19 +0530

    added continuous listening and languages

--- components/MQTTSpeechToText.jsx
+++ components/MQTTSpeechToText.jsx
@@ -6,10 +6,14 @@ import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import { useDashboardDataStore } from "@/context/MQTTContext";
-import { Button, Card, Col, Row } from "antd";
+import { Button, Card, Col, Row, Select } from "antd";
+
+import { languages } from "./languages";
 
 const MQTTSpeechToText = () => {
   const [start, setStart] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [lang, setLang] = useState("en-US");
   const {
     transcript,
     listening,
@@ -19,45 +23,73 @@ const MQTTSpeechToText = () => {
 
   const { mqttPublish } = useDashboardDataStore();
 
-  // useEffect(() => {
-  //   if (!listening && start) {
-  //     console.log("done");
-  //     mqttPublish({
-  //       topic: "oled_display",
-  //       payload: transcript,
-  //     });
-  //   }
-  //   if (!start && !listening) {
-  //     setStart(true);
-  //   }
-  // }, [listening]);
-
   useEffect(() => {
     if (transcript) {
+      setMsg(transcript);
       mqttPublish({
         topic: "oled_display",
         payload: transcript,
       });
     }
-  }, [transcript]);
-  
+
+    if (start || !listening) {
+      setTimeout(() => {
+        SpeechRecognition.startListening({ language: lang });
+      }, 100);
+    } else {
+      SpeechRecognition.stopListening();
+    }
+  }, [transcript, listening]);
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn&apos;t support speech recognition.</span>;
   }
 
+  //search
+  const onChange = (value) => {
+    setLang(value);
+  };
+
+  const filterOption = (input, option) =>
+    (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
+
   return (
-    <Card hoverable style={{ height: 400, width: 350 }}>
+    <Card hoverable style={{ height: 450, width: 350 }}>
       <center>
         <h3>Speech To Text To MQTT</h3>
       </center>
+      <center>
+        <Select
+          showSearch
+          placeholder="Select a Language"
+          optionFilterProp="children"
+          onChange={onChange}
+          filterOption={filterOption}
+          options={languages}
+        />
+      </center>
+      <br />
       <Row gutter={[16, 16]} justify="space-evenly">
         <Col>
-          {listening ? (
-            <Button type="primary" onClick={SpeechRecognition.stopListening}>
+          {start ? (
+            <Button
+              type="primary"
+              onClick={() => {
+                setStart(false);
+                SpeechRecognition.stopListening();
+              }}
+            >
               Stop
             </Button>
           ) : (
-            <Button type="primary" onClick={SpeechRecognition.startListening}>
+            <Button
+              type="primary"
+              onClick={() => {
+                
+                SpeechRecognition.startListening();
+                setStart(true);
+              }}
+            >
               Start
             </Button>
           )}
@@ -79,7 +111,7 @@ const MQTTSpeechToText = () => {
           borderRadius: 5,
         }}
       >
-        <p>{transcript}</p>
+        <p>{msg}</p>
       </div>
     </Card>
   );
